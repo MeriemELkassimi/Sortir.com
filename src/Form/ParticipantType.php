b8b1e11afb7e7497eb232a7ef521494e6e1b6e1c
Jean-François Tignon <jeanfrancois.tignon2023@campus-eni.fr>
2024-02-07 17:12:19 +0100

    Importmap fixtures de Campus ajout Villes par formulaire Formulaire : ParticipantType

---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            //->add('roles')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+           // ->add('telephone')
+           // ->add('isActif')
+            ->add('pseudo')
+            //->add('image')
+            ->add('sorties', EntityType::class, [
+                'class' => Sortie::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
